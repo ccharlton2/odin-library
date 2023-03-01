cdb022298aaaaaae9512b3018bf6d3a2596915b7
Christian <christiancharlton17@gmail.com>
2023-03-01 08:56:01 -0700

    Refactor to class

--- app.js
+++ app.js
@@ -1,107 +1,116 @@
-const library = [];
-const libraryContainer = document.querySelector(".library-container");
+const Library = (() => {
+  const libraryContainer = document.querySelector(".library-container");
+  const library = [];
 
-const bookForm = document.getElementById("book-form");
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const form = e.target;
-  const title = form.elements.title.value;
-  const author = form.elements.author.value;
-  const pages = form.elements.pages.value;
-  const hasRead = form.elements["has-read"].value === "on";
+  const bookForm = document.getElementById("book-form");
+  bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.elements.title.value;
+    const author = form.elements.author.value;
+    const pages = form.elements.pages.value;
+    const hasRead = form.elements["has-read"].checked;
 
-  const book = new Book(title, author, pages, hasRead);
+    const book = new Book(title, author, pages, hasRead);
 
-  addBook(book);
+    myLibrary.addBook(book);
 
-  fetchLibrary();
-});
+    myLibrary.render();
+  });
 
-const newBookButton = document.getElementById("new-book-button");
-newBookButton.addEventListener("click", () => {
-  bookForm.classList.toggle("hidden");
-  newBookButton.classList.toggle("hidden");
-});
+  const newBookButton = document.getElementById("new-book-button");
+  newBookButton.addEventListener("click", () => {
+    bookForm.classList.toggle("hidden");
+    newBookButton.classList.toggle("hidden");
+  });
 
-fetchLibrary();
-
-function Book(title, author, pages, hasRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.hasRead = hasRead;
-
-  this.getInfo = function getInfo() {
-    return `${title} by ${author}, ${
-      pages === 1 ? `${pages} Page` : `${pages} Pages`
-    }, ${
-      hasRead ? "You have read this book." : "You haven't read this book yet."
-    }`;
-  };
-}
-
-Book.prototype.setRead = function () {
-  if (this.hasRead) {
-    this.hasRead = false;
-  } else {
-    this.hasRead = true;
+  function addBook(book) {
+    library.push(book);
   }
-};
 
-function addBook(book) {
-  library.push(book);
-}
+  function render() {
+    libraryContainer.textContent = "";
+    if (library) {
+      for (let index = 0; index < library.length; index += 1) {
+        const bookId = index;
 
-function fetchLibrary() {
-  libraryContainer.textContent = "";
-  if (library) {
-    for (let index = 0; index < library.length; index += 1) {
-      const bookId = index;
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookCard.setAttribute("id", bookId);
 
-      const bookCard = document.createElement("div");
-      bookCard.classList.add("book-card");
-      bookCard.setAttribute("id", bookId);
+        const titleParagaph = document.createElement("p");
+        titleParagaph.textContent = `Title: ${library[index].title}`;
 
-      const titleParagaph = document.createElement("p");
-      titleParagaph.textContent = `Title: ${library[index].title}`;
+        const authorParagaph = document.createElement("p");
+        authorParagaph.textContent = `Author: ${library[index].author}`;
 
-      const authorParagaph = document.createElement("p");
-      authorParagaph.textContent = `Author: ${library[index].author}`;
+        const pagesParagaph = document.createElement("p");
+        pagesParagaph.textContent = `Pages: ${library[index].pages}`;
 
-      const pagesParagaph = document.createElement("p");
-      pagesParagaph.textContent = `Pages: ${library[index].pages}`;
+        const readCheckboxLabel = document.createElement("label");
+        readCheckboxLabel.textContent = "Read?";
+        readCheckboxLabel.setAttribute("for", `checkbox-${bookId}`);
 
-      const readCheckboxLabel = document.createElement("label");
-      readCheckboxLabel.textContent = "Read?";
-      readCheckboxLabel.setAttribute("for", `checkbox-${bookId}`);
+        const readCheckbox = document.createElement("input");
+        readCheckbox.setAttribute("type", "checkbox");
+        readCheckbox.setAttribute("id", `checkbox-${bookId}`);
+        readCheckbox.checked = library[index].hasRead;
 
-      const readCheckbox = document.createElement("input");
-      readCheckbox.setAttribute("type", "checkbox");
-      readCheckbox.setAttribute("id", `checkbox-${bookId}`);
-      readCheckbox.checked = library[index].hasRead;
+        readCheckbox.addEventListener("change", () => {
+          library[bookId].setRead();
 
-      readCheckbox.addEventListener("change", (e) => {
-        library[bookId].setRead();
+          render();
+        });
 
-        fetchLibrary();
-      });
+        const removeBookButton = document.createElement("button");
+        removeBookButton.classList.add("delete");
+        removeBookButton.textContent = "Delete";
+        removeBookButton.addEventListener("click", () => {
+          library.splice(index, 1);
+          render();
+        });
 
-      const removeBookButton = document.createElement("button");
-      removeBookButton.classList.add("delete");
-      removeBookButton.textContent = "Delete";
-      removeBookButton.addEventListener("click", () => {
-        library.splice(index, 1);
-        fetchLibrary();
-      });
+        bookCard.appendChild(titleParagaph);
+        bookCard.appendChild(authorParagaph);
+        bookCard.appendChild(pagesParagaph);
+        bookCard.appendChild(readCheckboxLabel);
+        bookCard.appendChild(readCheckbox);
+        bookCard.appendChild(removeBookButton);
 
-      bookCard.appendChild(titleParagaph);
-      bookCard.appendChild(authorParagaph);
-      bookCard.appendChild(pagesParagaph);
-      bookCard.appendChild(readCheckboxLabel);
-      bookCard.appendChild(readCheckbox);
-      bookCard.appendChild(removeBookButton);
-
-      libraryContainer.appendChild(bookCard);
+        libraryContainer.appendChild(bookCard);
+      }
     }
   }
-}
+
+  class Book {
+    constructor(title, author, pages, hasRead) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.hasRead = hasRead;
+    }
+
+    getInfo() {
+      return `${this.title} by ${this.author}, ${
+        this.pages === 1 ? `${this.pages} Page` : `${this.pages} Pages`
+      }, ${
+        this.hasRead
+          ? "You have read this book."
+          : "You haven't read this book yet."
+      }`;
+    }
+
+    setRead() {
+      if (this.hasRead) {
+        this.hasRead = false;
+      } else {
+        this.hasRead = true;
+      }
+    }
+  }
+  return { addBook, render };
+})();
+
+const myLibrary = Library;
+
+myLibrary.render();
